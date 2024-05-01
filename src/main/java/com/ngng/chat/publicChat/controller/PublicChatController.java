d075e94ab061673dd1f1ca2a6dabe3fab29268a3
chomyungha <chomyungha@gmail.com>
2024-05-01 09:22:36 +0900

    FIX : controller cors 수정

--- src/main/java/com/ngng/chat/publicChat/controller/PublicChatController.java
+++ src/main/java/com/ngng/chat/publicChat/controller/PublicChatController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
